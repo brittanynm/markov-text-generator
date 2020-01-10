cdcf4cbe5eda8a770c16ea8fd607be86c0456c4f
Brittany Morton <bnmorton264@gmail.com>
2020-01-10 11:58:54 -0800

    V1

--- markov.py
+++ markov.py
@@ -1,5 +1,6 @@
 """Generate Markov text from text files."""
 
+import sys
 from random import choice
 
 
@@ -10,9 +11,13 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
-    # your code goes here
+    text = ''
+    for file in filepath:
+        text_file = open(file)
+        text = text + text_file.read()
+        text_file.close()
 
-    return "Contents of your file as one long string"
+    return text
 
 
 def make_chains(text_string):
@@ -42,7 +47,16 @@ def make_chains(text_string):
 
     chains = {}
 
-    # your code goes here
+    words = text_string.split()
+
+    for i in range(len(words) - 2):
+        key = (words[i], words[i + 1])
+        value = words[i + 2]
+
+        if key not in chains:
+            chains[key] = []
+
+        chains[key].append(value)
 
     return chains
 
@@ -50,14 +64,21 @@ def make_chains(text_string):
 def make_text(chains):
     """Return text from chains."""
 
-    words = []
+    key = choice(list(chains.keys()))
+    words = [key[0], key[1]]
+    word = choice(chains[key])
 
-    # your code goes here
+    # Loop until we reach a value of None
+    # (which would mean the end of our text)
+    while word is not None:
+        key = (key[1], word)
+        words.append(word)
+        word = choice(chains[key])
 
     return " ".join(words)
 
 
-input_path = "green-eggs.txt"
+input_path = sys.argv[1]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
